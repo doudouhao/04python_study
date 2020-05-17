19e0a40e9c4c71ce18105f4d63fec067640fe9d9
doudouhao <1361481896@qq.com>
2020-05-17 15:30:39 +0200

    study the diffience between the Amend commit and sign-off commit https://blog.csdn.net/jankingmeaning/article/details/104447275

---
+++ ifelse.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+# Author:wanghao
+# date:2020年5月17日
+height = 1.75
+weight = 80.5
+bmi = weight / (height * height)
+# if bmi < 18.5:
+#     print("过轻")
+# elif   25 > bmi >= 18.5:
+#     print("正常")
+# elif  28 > bmi >= 25:
+#     print("过重")
+# elif  32 > bmi >= 28:
+#     print("肥胖")
+# else:
+#     print("严重肥胖")
+
+if 18.5 > bmi:
+    print("过轻")
+elif 18.5 <= bmi < 25:
+    print("正常")
+elif 28 > bmi >= 25:
+    print("过重")
+elif 32 > bmi >= 28:
+    print("肥胖")
+else:
+    print("严重肥胖")
